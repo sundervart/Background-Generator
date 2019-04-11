51fe9277bffd32d756ab02634579811530e789f2
Stéphane <stephanesilva1992@outlook.com>
2019-04-11 23:17:53 +0200

    BEautify it

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var currentBackground = document.querySelector("h3");
-var color1 =  document.querySelector("#color1");
+var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var body = document.querySelector("#background");
 var randomButton = document.querySelector("#random");
@@ -8,35 +8,33 @@ color1.value = "#ff0000";
 color2.value = "#ffff00";
 currentBackground.textContent = "linear-gradient(to right, red , yellow);";
 
-function generateNum (){
+function generateNum() {
     var result = "";
-    for (var i = 1; i<=3; i++){
-        if(i === 1){
-        var num = Math.floor((Math.random() * 255) + 1);
-        result = result + "rgb(" + num + ", ";
-        }
-        else if( i === 2){
+    for (var i = 1; i <= 3; i++) {
+        if (i === 1) {
+            var num = Math.floor((Math.random() * 255) + 1);
+            result = result + "rgb(" + num + ", ";
+        } else if (i === 2) {
             var num = Math.floor((Math.random() * 255) + 1);
             result = result + num + ", ";
-        }
-        else if (i === 3){
+        } else if (i === 3) {
             num = Math.floor((Math.random() * 255) + 1);
             result = result + num + ")"
         }
-        
+
     }
     return result;
 }
 
-function randomGradiant (){
+function randomGradiant() {
     var one = generateNum()
     var two = generateNum()
     body.style.background = "linear-gradient(to right, " + one + ", " + two + ")";
     currentBackground.textContent = body.style.background + ";";
-    
+
 }
 
-function setBackground(){
+function setBackground() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
     currentBackground.textContent = body.style.background + ";";
 }
@@ -44,4 +42,4 @@ function setBackground(){
 color1.addEventListener("input", setBackground);
 color2.addEventListener("input", setBackground);
 
-randomButton.addEventListener("click",randomGradiant)
+randomButton.addEventListener("click", randomGradiant)
